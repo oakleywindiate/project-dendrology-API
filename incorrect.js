b0f34ec604078e76d60c802a9533fc5c9d6f2be4
Oakley Windiate <oakley.windiate@gmail.com>
2022-06-10 01:22:37 -0600

    Create pathways for POST

---
+++ incorrect.js
@@ -0,0 +1,5 @@
+const incorrectData = [
+
+]
+
+module.exports = incorrectData;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 questionData = require('./data')
+incorrectData = require('./incorrect')
+
 const cors = require('cors');
 const express = require('express');
 const app = express();
 
 app.use(cors())
+
 app.locals.questions = questionData;
+app.locals.incorrect = incorrectData;
 
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'project+dendrology API';
@@ -14,9 +18,13 @@ app.listen(app.get('port'), () => {
 });
 
 app.get('/', (request, response) => {
+    response.send('Successful');
+});  
+
+app.get('/test', (request, response) => {
     response.status(200).json(app.locals.questions);
 });
 
-app.get('/', (request, response) => {
-    response.send('Successful');
-});  
+app.get('/incorrect', (request, response) => {
+    response.status(200).json(app.locals.incorrect);
+});
